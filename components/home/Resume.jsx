7f03ae476c156739736935a84d2b3c222ccb703c
Saman Manesh <samanmanesh7@gmail.com>
2021-07-02 20:01:55 -0400

    did the resume

--- components/home/Resume.jsx
+++ components/home/Resume.jsx
@@ -4,14 +4,16 @@ import styled from "styled-components";
 
 const EducationGrid = styled.section``;
 const ResumeCard = styled.div``;
+const Skills = styled.section``;
+
 export default function Resume() {
   return (
     <Container>
       <Title3>Resume</Title3>
       <img src="" alt="arrow type3" className="arrow-type3" />
 
-      <Title2>Education {"{"} </Title2>
       <EducationGrid>
+        <Title2>Education {"{"} </Title2>
         <div className="left-side">
           <ResumeCard>
             <div className="card-row">
@@ -52,8 +54,45 @@ export default function Resume() {
             </div>
           </ResumeCard>
         </div>
+        <Title2>{"}"} </Title2>
       </EducationGrid>
-      <Title2>{"}"} </Title2>
+
+      <Skills>
+        <Title2>Skills {"{"}</Title2>
+        <div className="skill-container-grid">
+          <div className="element">HTML 5</div>
+          <div className="element">CSS 3</div>
+          <div className="element">SASS</div>
+          <div className="element">TailWind</div>
+          <div className="element">JavaScript</div>
+          <div className="element">ReactJS</div>
+          <div className="element">VueJS</div>
+          <div className="element">NextJS</div>
+          <div className="element">UI / UX Design</div>
+          <div className="element">C</div>
+          <div className="element">Linux 7&amp; Unix</div>
+          <div className="element">C#</div>
+          <div className="element">Styled Components</div>
+        </div>
+        <Title2> {"}"}</Title2>
+      </Skills>
+      <Tools>
+        <Title2>Tools {"{"}</Title2>
+        <div className="tools-container-grid">
+          <div className="element">Visual Studio Code</div>
+          <div className="element">Rider</div>
+          <div className="element">Unity</div>
+          <div className="element">Vi Editor</div>
+          <div className="element">Framer</div>
+          <div className="element">Figma</div>
+        </div>
+      <Title2>Tools {"}"}</Title2>
+      </Tools>
+      <button className="download-resume">
+        <span>Download Reśumé</span>
+        <img src="" alt="array type 4" className="arrow-type4" />
+      </button>
+
     </Container>
   );
 }
