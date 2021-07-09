7771f502bf41a2beb05025757080f769fa38fec8
Saman Manesh <samanmanesh7@gmail.com>
2021-07-09 11:02:29 -0400

    nav check

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState} from "react";
 import styled from "styled-components";
 import { mobile, tablet, colors } from "./styles/design";
 
@@ -10,34 +10,16 @@ const Nav = styled.nav`
   font-weight: 100;
   border-bottom: 1.5px solid white;
   /* margin-inline: 1rem; */
-  font-size: calc(.7rem + .4vw);
+  font-size: calc(0.7rem + 0.4vw);
   /* font-size: clamp(0.9rem, 1.1vw, 1.3rem); */
   > * {
-    padding: 1.5em;
-  }
-
-  @media (max-width: ${tablet}) {
-    position: fixed;
-    background: ${colors.background};
-    border-bottom: 1px solid white;
-    z-index: 100;
-    //Todo adding feature when you scroll up nav disappear 
-    > * {
-      font-size:1rem;
-      /* padding-inline: 5em; */
-    }
-    .nav-item{
-      padding-inline: 2rem;
-      background: #8d2323;
-    }
-
+    padding-block: 1.5em;
   }
 
   .anime-function-container {
-    /* font-size: clamp(0.7em, 0.7vw, 1em); */
-   font-size: calc(.4rem + .7vw);
+    font-size: calc(0.4rem + 0.7vw);
     align-self: center;
-    /* background: #3737a4; */
+    padding-left:.4rem;
   }
 
   .nav-items-container {
@@ -49,18 +31,51 @@ const Nav = styled.nav`
     .nav-item {
       cursor: pointer;
       padding-inline: 1rem;
+
       :hover {
         transition: all 0.5s ease;
         font-weight: bold;
         font-size: 1rem;
       }
     }
-  }
+  
 
-  @media (max-width: ${mobile}) {
+  /* @media (max-width: ${mobile}) {
     .anime-function-container {
       display: none;
-      /* background: #1f4d77; */
+    }
+    .nav-items-container {
+      flex-grow: 1;
+      margin: 0;
+    }
+  } */
+
+  /* @media (max-width: ${tablet}) {
+    .anime-function-container {
+      /* font-size: calc(.2rem + .5vw); */
+    /* } */
+    .nav-items-container {
+      margin: 0;
+    }
+  }
+
+  @media (max-width: ${tablet}) {
+    position: fixed;
+    background: ${colors.background};
+    border-bottom: 1px solid white;
+    z-index: 100;
+   
+    //Todo adding feature when you scroll up nav disappear
+    > * {
+      font-size: 1rem;
+    }
+    
+  }
+
+  @media (max-width: ${mobile}){
+    width:98%;
+    .anime-function-container {
+      display: none;
     }
     .nav-items-container {
       flex-grow: 1;
@@ -68,27 +83,36 @@ const Nav = styled.nav`
     }
   }
 
-  @media (max-width: ${tablet}) {
-    .anime-function-container {
-      /* background: #5e771f; */
-    }
-    .nav-items-container {
-      margin: 0;
-    }
-  }
-  
+
+
+
 `;
 //#endregion
 export default function NavBar() {
+  // const [codeState, setCodeState] = useState("Home");
+
   const scrollHandler = (id) => {
     id.scrollIntoView({ behavior: "smooth" });
+    
+    // if (id === work) {
+    //   setCodeState("Work");
+    // }else if (id === resume){
+    //   setCodeState("Resume");
+    // }else if (id ===about){
+    //   setCodeState("About");
+    // }
+    // else if (id === contact){
+    //   setCodeState("Contact");
+    // }
+
   };
 
   return (
     <Nav>
       <div className="anime-function-container">
-        const {"hero"} = ( ) ={">"} {"{display hero}"}
-        {/* SAMAN MANESH */}
+        {/* const {codeState} = ( ) ={">"} {"{display"} {codeState}}
+        {"}"}; */}
+        SAMAN MANESH
       </div>
 
       <div className="nav-items-container">
