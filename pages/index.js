1c0a9a9383deb156840877c0e46bd07f1b5085c4
Saman Manesh <samanmanesh7@gmail.com>
2021-07-23 10:47:25 -0400

    added Head to index to see if it works!

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,28 @@ import Hero from "../components/home/Hero";
 import Work from "../components/home/Work";
 import Resume from "../components/home/Resume";
 import About from "../components/home/About";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <PageBase>
+      <Head>
+        <title>Saman Manesh</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <meta
+          property="og:title"
+          content="Saman Manesh's personal website/portfolio"
+          key="title"
+        />
+        <meta property="og:image" content="//www.samanmanesh.com/hero.png" />
+        <meta
+          property="og:description"
+          content="I’m Saman Manesh a Full-Stack Developer located in Toronto...I have a serious passion for UI effects, animations and creating intuitive, dynamic user experiences..."
+        />
+        
+      </Head>
         <Hero />
         <Work />
         <Resume />
