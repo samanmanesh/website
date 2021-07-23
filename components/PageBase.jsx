e40c7618c0d0627aef779d169586c424a50e4662
Saman Manesh <samanmanesh7@gmail.com>
2021-07-22 20:17:49 -0400

    changed meta tag

--- components/PageBase.jsx
+++ components/PageBase.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import AlertBox from "./home/AlertBox";
 import styled from "styled-components";
 import colors from "./styles/design";
-
 
 const PageBaseWrapper = styled.div`
   display: flex;
@@ -23,18 +22,31 @@ export default function PageBase({ children }) {
     setTimeout(() => {
       setAlert(null);
     }, 5000);
-  }, [alert])
+  }, [alert]);
 
   return (
     <PageBaseWrapper>
       <Head>
         <title>Saman Website</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        <meta name="description" content="I’m Saman Manesh a Full-Stack Developer located in Toronto...I have a serious passion for UI effects, animations and creating intuitive, dynamic user experiences...">
-        </meta>
-        <meta property='og:image' content='//www.samanmanesh.com/hero.png' />
-        <meta property="og:title" content="Saman Manesh's personal website/portfolio" key="ogtitle" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+
+        <meta
+          property="og:title"
+          content="Saman Manesh's personal website/portfolio"
+        />
+        <meta property="og:image" content="//www.samanmanesh.com/ hero.png" />
+        <meta
+          property="og:description"
+          content="I’m Saman Manesh a Full-Stack Developer located in Toronto...I have a serious passion for UI effects, animations and creating intuitive, dynamic user experiences..."
+        />
+        <meta
+          property="og:url"
+          content="//www.example.com/URL of the article"
+        />
       </Head>
       <NavBar />
       <AlertBox alert={alert} />
